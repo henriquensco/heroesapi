3e5063c795e036017afd355cb3e57591591ef6e2
Henrique <nascimentocostahenrique@gmail.com>
2021-05-12 09:51:16 -0300

    Heroes Api - Springboot and WebFlux

---
+++ src/main/java/com/digitalinnovationone/heroesapi/document/Heroes.java
@@ -0,0 +1,66 @@
+package com.digitalinnovationone.heroesapi.document;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBAttribute;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBHashKey;
+import org.springframework.data.annotation.Id;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBTable;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@DynamoDBTable(tableName = "Heroes_Table")
+public class Heroes {
+    @Id
+    @DynamoDBHashKey(attributeName = "id")
+    private String id;
+
+    @DynamoDBAttribute(attributeName = "name")
+    private String name;
+
+    @DynamoDBAttribute(attributeName = "universe")
+    private String universe;
+
+    @DynamoDBAttribute(attributeName = "filmes")
+    private Integer filmes;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUniverse() {
+        return universe;
+    }
+
+    public void setUniverse(String universe) {
+        this.universe = universe;
+    }
+
+    public Integer getFilmes() {
+        return filmes;
+    }
+
+    public void setFilmes(Integer filmes) {
+        this.filmes = filmes;
+    }
+
+    public Heroes(String id, String name, String universe, Integer filmes) {
+        this.id = id;
+        this.name = name;
+        this.universe = universe;
+        this.filmes = filmes;
+    }
+}
